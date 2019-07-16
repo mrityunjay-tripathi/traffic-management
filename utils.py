fa29578ed06d39a5ddde183e5d4fa29e778f3e51
Mrityunjay Tripathi <mrityunjay2668@gmail.com>
2019-07-16 08:58:42 +0530

    updated utils

--- utils.py
+++ utils.py
@@ -1,9 +1,40 @@
 import os
 import pandas as pd
 import xml.etree.ElementTree as ET
+from PIL import Image
+
+def rename_multiple_files(path):
+
+    i=0
+
+    for filename in os.listdir(path):
+        try:
+            f,extension = os.path.splitext(path+filename)
+            src=path+filename
+            dst=path+str(i)+extension
+            os.rename(src,dst)
+            i+=1
+            print('Rename successful.')
+        except:
+            i+=1
+            print('Unable to rename.')
 
 
-def convert(path):
+            
+def resize_multiple_images(src_path, dst_path):
+    # Here path is the location where images are saved.
+    for filename in os.listdir(src_path):
+        try:
+            img=Image.open(src_path+filename)
+            new_img = img.resize((832,624))
+            if not os.path.exists(dst_path):
+                os.makedirs(dst_path)
+            new_img.save(dst_path+filename)
+            print('Resized and saved {} successfully.'.format(filename))
+        except:
+            continue
+
+def xml_to_csv(path):
     xml_list = []
     for filename in os.listdir(path):
         root = ET.parse(path + filename).getroot()
@@ -32,6 +63,4 @@ def convert(path):
     return xml_df
 
 
-df = convert('dataset/xml_details/')
-df.to_csv('dataset/labelled_data.csv', index = False)
-print(df)
+
