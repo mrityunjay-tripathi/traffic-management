41b01318e2666b20a2a84304f46bb622e7201144
Mrityunjay Tripathi <mrityunjay2668@gmail.com>
2019-11-24 13:36:20 +0530

    24-11-2019

--- utils/other_utils.py
+++ utils/other_utils.py
@@ -11,7 +11,7 @@ def rename_multiple_files(path):
     print('Processing...')
     for filename in os.listdir(path):
         try:
-            f,extension = os.path.splitext(path+filename)
+            _, extension = os.path.splitext(path+filename)
             src=path+filename
             dst=path+str(name)+extension
             os.rename(src,dst)
