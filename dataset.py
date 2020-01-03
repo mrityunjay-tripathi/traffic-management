4ea238529723f6c2e51672f9a00afed83244aa13
Mrityunjay Tripathi <mrityunjay2668@gmail.com>
2020-01-03 13:59:51 +0530

    implemented faster extraction of images and annotations

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
-import os, torch
+import os, torch, glob
 from PIL import Image
 from torch.utils.data import Dataset
+from torchvision import transforms
 from YOLOUtils import ProcessedBB, TrueBoxes
 
 class NumberPlateDataset(Dataset):
@@ -9,39 +10,30 @@ class NumberPlateDataset(Dataset):
                  train_images_path, 
                  annotations_path,
                  anchors,
-                 transform = transforms.Compose([transforms.ToTensor()])):
+                 transform):
         self.train_images_path = train_images_path
         self.annotations_path = annotations_path
         self.transform = transform
-        self.class_dict = {"license plate":1}
         self.image_shape = (480, 640)
         self.grid_shape = (12, 16)
         self.num_of_classes = 1
         self.anchors = anchors
-    
-    def files(self):
-        f = []
-        for filename in os.listdir(self.annotations_path):
-            f.append(filename)
-        return f
+        self.xml_files = sorted(glob.glob(f"{annotations_path}/*.xml"))
+        self.image_files = sorted(glob.glob(f"{train_images_path}/*.jpg"))
 
     def __len__(self):
-        return len(self.files())
+        return len(self.xml_files)
     
-
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = torch.tolist()
-        f = self.files()
-        xml_path = f[idx]
-        boxes = TrueBoxes(os.path.join(self.annotations_path, xml_path))
+        boxes = TrueBoxes(self.xml_files[idx%len(self.xml_files)])
         true_boxes = ProcessedBB(boxes=boxes,
                                  anchors = self.anchors,
                                  image_shape = self.image_shape,
                                  grid_shape = self.grid_shape,
-                                 num_of_classes = self.num_of_classes)
-        img = Image.open(self.train_images_path + f[idx][:-3] + 'jpg')
-
+                                 num_classes = self.num_of_classes)
+        img = Image.open(self.image_files[idx%len(self.image_files)])
         if self.transform:
             img = self.transform(img)
         return {'image':img, 'true_boxes':true_boxes}
